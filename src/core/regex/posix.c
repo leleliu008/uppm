f067700f03235c96de51ad1d982947dae76f0e36
leleliu008 <leleliu008@gmail.com>
2022-09-16 08:38:28 +0800

    optimized

--- src/core/regex/posix.c
+++ src/core/regex/posix.c
@@ -3,6 +3,8 @@
 #include <regex.h>
 
 bool regex_match(const char * content, const char * pattern) {
+    //printf("content = %s\n", content);
+    //printf("pattern = %s\n", pattern);
     if (content == NULL || pattern == NULL) {
         return false;
     }
@@ -11,7 +13,7 @@ bool regex_match(const char * content, const char * pattern) {
 
     if (regcomp(&regex, pattern, REG_EXTENDED) == 0) {
         regmatch_t regmatch[2];
-        if (regexec(&regex, pattern, 2, regmatch, 0) == 0) {
+        if (regexec(&regex, content, 2, regmatch, 0) == 0) {
             //printf("regmatch[0].rm_so=%d\n", regmatch[0].rm_so);
             //printf("regmatch[0].rm_eo=%d\n", regmatch[0].rm_eo);
             if (regmatch[0].rm_so >= 0 && regmatch[0].rm_eo > regmatch[0].rm_so) {
