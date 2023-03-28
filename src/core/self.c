b8c6a50a217d5aa4056954d7bbc678516d456847
leleliu008 <leleliu008@gmail.com>
2023-03-28 22:13:14 +0800

    optimized
    Signed-off-by: leleliu008 <leleliu008@gmail.com>

--- src/core/self.c
+++ src/core/self.c
@@ -5,7 +5,9 @@
 #include <unistd.h>
 #include <sys/stat.h>
 
+// https://developer.apple.com/library/archive/documentation/System/Conceptual/ManPages_iPhoneOS/man3/dyld.3.html
 #if defined (__APPLE__)
+#include <mach-o/dyld.h>
 #include <sys/syslimits.h>
 #elif defined (__linux__) && defined (HAVE_LINUX_LIMITS_H)
 #include <linux/limits.h>
