860d4216ddee774d1860c245b09ccdadcd7b23cf
leleliu008 <leleliu008@gmail.com>
2026-08-28 10:04:04 +0800

    optimized

--- src/core/linux/determine-linux-system-libc.c
+++ src/core/linux/determine-linux-system-libc.c
@@ -1,202 +1,88 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
 
 #include <unistd.h>
 #include <fcntl.h>
+
 #include <sys/stat.h>
+#include <sys/mman.h>
 
 #include <elf.h>
 
 #define ERROR_STAT               100
 #define ERROR_OPEN               101
-#define ERROR_SEEK               102
-#define ERROR_READ               103
-#define ERROR_NOT_FULLY_READ     104
+#define ERROR_MMAP               102
 
 #define ERROR_NOT_ELF_FILE       120
 #define ERROR_BROKEN_ELF_FILE    130
-#define ERROR_NOT_DETERMINED     200
 
+#define LIBC_UNKNOWN 0
 #define LIBC_GNU     1
 #define LIBC_MUSL    2
 
-static int handle_elf32(const int fd, const char * const fp) {
-    Elf32_Ehdr ehdr;
+/*
+ * default dynamic loaders for glibc:
+ *
+ * x86_64     /lib64/ld-linux-x86-64.so.2
+ * aarch64    /lib/ld-linux-aarch64.so.1
+ * riscv64    /lib/ld-linux-riscv64-lp64d.so.1
+ * armhf      /lib/ld-linux-armhf.so.3
+ * ppc64le    /lib/powerpc64le-linux-gnu/ld64.so.2
+ * ppc64le    /lib64/ld64.so.2
+ * s390x      /lib/ld64.so.1
+ */
 
-    int ret = read(fd, &ehdr, sizeof(Elf32_Ehdr));
+/*
+ * default dynamic loaders for musl:
+ *
+ * armhf       /lib/ld-musl-armhf.so.1
+ * x86         /lib/ld-musl-i386.so.1
+ * s390x       /lib/ld-musl-s390x.so.1
+ * x86_64      /lib/ld-musl-x86_64.so.1
+ * aarch64     /lib/ld-musl-aarch64.so.1
+ * riscv64     /lib/ld-musl-riscv64.so.1
+ * ppc64le     /lib/ld-musl-powerpc64le.so.1
+ * loongarch64 /lib/ld-musl-loongarch64.so.1
+ */
 
-    if (ret == -1) {
-        perror(fp);
-        return ERROR_READ;
-    }
+static inline __attribute__((always_inline)) int handle_elf64(const unsigned char * elf) {
+    Elf64_Ehdr *ehdr = (Elf64_Ehdr *)elf;
+    Elf64_Phdr *phdr = (Elf64_Phdr *)(elf + ehdr->e_phoff);
 
-    if (ret != sizeof(Elf32_Ehdr)) {
-        perror(fp);
-        return ERROR_NOT_FULLY_READ;
-    }
+    for (Elf64_Half i = 0; i < ehdr->e_phnum; i++) {
+        if (phdr[i].p_type == PT_INTERP) {
+            const char * p = (const char *)(elf + phdr[i].p_offset);
 
-    ///////////////////////////////////////////////////////////
+            //fprintf(stderr, "interpreter : %s\n", p);
 
-    Elf32_Phdr phdr;
-
-    for (unsigned int i = 1; i < ehdr.e_phnum; i++) {
-        ret = pread(fd, &phdr, sizeof(Elf32_Phdr), ehdr.e_phoff + i * ehdr.e_phentsize);
-
-        if (ret == -1) {
-            perror(fp);
-            return ERROR_READ;
-        }
-
-        if ((size_t)ret != sizeof(Elf32_Phdr)) {
-            perror(fp);
-            fprintf(stderr, "not fully read.\n");
-            return ERROR_NOT_FULLY_READ;
-        }
-
-        if (phdr.p_type != PT_INTERP) {
-            continue;
-        }
-
-        char p[phdr.p_filesz];
-
-        ret = pread(fd, p, phdr.p_filesz, phdr.p_offset);
-
-        if (ret == -1) {
-            perror(fp);
-            return ERROR_READ;
-        }
-
-        if ((size_t)ret != phdr.p_filesz) {
-            perror(fp);
-            fprintf(stderr, "not fully read.\n");
-            return ERROR_NOT_FULLY_READ;
-        }
-
-        fprintf(stderr, "%s interpreter : %s\n", fp, p);
-
-        /*
-         * default dynamic loaders for glibc:
-         *
-         * x86_64     /lib64/ld-linux-x86-64.so.2
-         * aarch64    /lib/ld-linux-aarch64.so.1
-         * riscv64    /lib/ld-linux-riscv64-lp64d.so.1
-         * armhf      /lib/ld-linux-armhf.so.3
-         * ppc64le    /lib/powerpc64le-linux-gnu/ld64.so.2
-         * ppc64le    /lib64/ld64.so.2
-         * s390x      /lib/ld64.so.1
-         */
-
-        /*
-         * default dynamic loaders for musl:
-         *
-         * armhf       /lib/ld-musl-armhf.so.1
-         * x86         /lib/ld-musl-i386.so.1
-         * s390x       /lib/ld-musl-s390x.so.1
-         * x86_64      /lib/ld-musl-x86_64.so.1
-         * aarch64     /lib/ld-musl-aarch64.so.1
-         * riscv64     /lib/ld-musl-riscv64.so.1
-         * ppc64le     /lib/ld-musl-powerpc64le.so.1
-         * loongarch64 /lib/ld-musl-loongarch64.so.1
-         */
-
-        if (strncmp(p, "/lib/ld-musl-", 13) == 0) {
-            return LIBC_MUSL;
-        } else {
-            return LIBC_GNU;
+            if (strncmp(p, "/lib/ld-musl-", 13) == 0) {
+                return LIBC_MUSL;
+            } else {
+                return LIBC_GNU;
+            }
         }
     }
 
-    return ERROR_NOT_DETERMINED;
+    return LIBC_UNKNOWN;
 }
 
-static int handle_elf64(const int fd, const char * const fp) {
-    Elf64_Ehdr ehdr;
+static inline __attribute__((always_inline)) int handle_elf32(const unsigned char * elf) {
+    Elf32_Ehdr *ehdr = (Elf32_Ehdr *)elf;
+    Elf32_Phdr *phdr = (Elf32_Phdr *)(elf + ehdr->e_phoff);
 
-    int ret = read(fd, &ehdr, sizeof(Elf64_Ehdr));
+    for (Elf32_Half i = 0; i < ehdr->e_phnum; i++) {
+        if (phdr[i].p_type == PT_INTERP) {
+            const char * p = (const char *)(elf + phdr[i].p_offset);
 
-    if (ret == -1) {
-        perror(fp);
-        return ERROR_READ;
-    }
-
-    if (ret != sizeof(Elf64_Ehdr)) {
-        perror(fp);
-        return ERROR_NOT_FULLY_READ;
-    }
-
-    ///////////////////////////////////////////////////////////
-
-    Elf64_Phdr phdr;
-
-    for (unsigned int i = 1; i < ehdr.e_phnum; i++) {
-        ret = pread(fd, &phdr, sizeof(Elf64_Phdr), ehdr.e_phoff + i * ehdr.e_phentsize);
-
-        if (ret == -1) {
-            perror(fp);
-            return ERROR_READ;
-        }
-
-        if ((size_t)ret != sizeof(Elf64_Phdr)) {
-            perror(fp);
-            fprintf(stderr, "not fully read.\n");
-            return ERROR_NOT_FULLY_READ;
-        }
-
-        if (phdr.p_type != PT_INTERP) {
-            continue;
-        }
-
-        char p[phdr.p_filesz];
-
-        ret = pread(fd, p, phdr.p_filesz, phdr.p_offset);
-
-        if (ret == -1) {
-            perror(fp);
-            return ERROR_READ;
-        }
-
-        if ((size_t)ret != phdr.p_filesz) {
-            perror(fp);
-            fprintf(stderr, "not fully read.\n");
-            return ERROR_NOT_FULLY_READ;
-        }
-
-        fprintf(stderr, "%s interpreter : %s\n", fp, p);
-
-        /*
-         * default dynamic loaders for glibc:
-         *
-         * x86_64     /lib64/ld-linux-x86-64.so.2
-         * aarch64    /lib/ld-linux-aarch64.so.1
-         * riscv64    /lib/ld-linux-riscv64-lp64d.so.1
-         * armhf      /lib/ld-linux-armhf.so.3
-         * ppc64le    /lib/powerpc64le-linux-gnu/ld64.so.2
-         * ppc64le    /lib64/ld64.so.2
-         * s390x      /lib/ld64.so.1
-         */
-
-        /*
-         * default dynamic loaders for musl:
-         *
-         * armhf       /lib/ld-musl-armhf.so.1
-         * x86         /lib/ld-musl-i386.so.1
-         * s390x       /lib/ld-musl-s390x.so.1
-         * x86_64      /lib/ld-musl-x86_64.so.1
-         * aarch64     /lib/ld-musl-aarch64.so.1
-         * riscv64     /lib/ld-musl-riscv64.so.1
-         * ppc64le     /lib/ld-musl-powerpc64le.so.1
-         * loongarch64 /lib/ld-musl-loongarch64.so.1
-         */
-
-        if (strncmp(p, "/lib/ld-musl-", 13) == 0) {
-            return LIBC_MUSL;
-        } else {
-            return LIBC_GNU;
+            if (strncmp(p, "/lib/ld-musl-", 13) == 0) {
+                return LIBC_MUSL;
+            } else {
+                return LIBC_GNU;
+            }
         }
     }
 
-    return ERROR_NOT_DETERMINED;
+    return LIBC_UNKNOWN;
 }
 
 static int determine_by_inspect_elf_file(const char * fp) {
@@ -219,58 +105,36 @@ static int determine_by_inspect_elf_file(const char * fp) {
         return ERROR_OPEN;
     }
 
-    ///////////////////////////////////////////////////////////
+    void * data = mmap(NULL, st.st_size, PROT_READ, MAP_PRIVATE, fd, 0);
 
-    unsigned char a[5];
-
-    ssize_t readBytes = read(fd, a, 5);
-
-    if (readBytes == -1) {
-        perror(fp);
+    if (data == MAP_FAILED) {
+        perror("mmap");
         close(fd);
-        return ERROR_READ;
-    }
-
-    if (readBytes != 5) {
-        perror(fp);
-        close(fd);
-        fprintf(stderr, "not fully read.\n");
-        return ERROR_NOT_FULLY_READ;
-    }
-
-    ///////////////////////////////////////////////////////////
-
-    // https://www.sco.com/developers/gabi/latest/ch4.eheader.html
-    if ((a[0] != 0x7F) || (a[1] != 0x45) || (a[2] != 0x4C) || (a[3] != 0x46)) {
-        //fprintf(stderr, "NOT an ELF file: %s\n", fp);
-        close(fd);
-        return ERROR_NOT_ELF_FILE;
-    }
-
-    ///////////////////////////////////////////////////////////
-
-    off_t offset = lseek(fd, 0, SEEK_SET);
-
-    if (offset == -1) {
-        perror(fp);
-        close(fd);
-        return ERROR_SEEK;
-    }
-
-    ///////////////////////////////////////////////////////////
-
-    int ret;
-
-    switch (a[4]) {
-        case ELFCLASS32: ret = handle_elf32(fd, fp); break;
-        case ELFCLASS64: ret = handle_elf64(fd, fp); break;
-        default: 
-            //fprintf(stderr, "Invalid ELF file: %s\n", fp);
-            ret = ERROR_BROKEN_ELF_FILE;
+        return ERROR_MMAP;
     }
 
     close(fd);
 
+    unsigned char * elf = (unsigned char *)data;
+
+    if (memcmp(elf, ELFMAG, SELFMAG) != 0) {
+        fprintf(stderr, "Not an ELF file: %s\n", fp);
+        munmap(data, st.st_size);
+        close(fd);
+        return ERROR_NOT_ELF_FILE;
+    }
+
+    int ret;
+
+    switch (elf[EI_CLASS]) {
+        case ELFCLASS64: ret = handle_elf64(elf); break;
+        case ELFCLASS32: ret = handle_elf32(elf); break;
+        default:
+            fprintf(stderr, "invalid ELF file: %s\n", fp);
+            ret = ERROR_BROKEN_ELF_FILE;
+    }
+
+    munmap(data, st.st_size);
     return ret;
 }
 
